f9510ec51524f6b6ec78fce3283dcbb243f2749d
om pandey <om.pandey@aircall.io>
2022-01-24 01:19:17 +0530

    typrscript integration

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-const Joi = require("joi");
-const express = require("express");
+import express from "express";
+
 const app = express();
 
 app.use(express.json());
-const courses = [
+const courses: any = [
   { id: 1, name: "course1" },
-  { id: 2, name: "course1" },
-  { id: 3, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
 ];
 
 app.get("/", (req, res) => {
@@ -18,11 +18,6 @@ app.get("/api/courses", (req, res) => {
 });
 
 app.post("/api/courses", (req, res) => {
-  const { error } = validateCourse(req.body);
-  if (error) {
-    //   400 Bad Request
-    return res.status(400).send(error.details[0].message);
-  }
   const course = {
     id: courses.length + 1,
     name: req.body.name,
@@ -32,20 +27,16 @@ app.post("/api/courses", (req, res) => {
 });
 
 app.put("/api/courses/:id", (req, res) => {
-  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  const course = courses.find((c: any) => c.id === parseInt(req.params.id));
   if (!course)
     return res.status(404).send("The Course with given ID was not found.");
-  const { error } = validateCourse(req.body);
-  if (error) {
-    //   400 Bad Request
-    return res.status(400).send(error.details[0].message);
-  }
+
   course.name = req.body.name;
   res.send(course);
 });
 
 app.delete("/api/courses/:id", (req, res) => {
-  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  const course = courses.find((c: any) => c.id === parseInt(req.params.id));
   if (!course)
     return res.status(404).send("The Course with given ID was not found.");
 
@@ -54,15 +45,8 @@ app.delete("/api/courses/:id", (req, res) => {
   res.send(course);
 });
 
-function validateCourse(course) {
-  const schema = {
-    name: Joi.string().min(3).required(),
-  };
-  return Joi.validate(course, schema);
-}
-
 app.get("/api/courses/:id", (req, res) => {
-  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  const course = courses.find((c: any) => c.id === parseInt(req.params.id));
   if (!course)
     return res.status(404).send("The Course with given ID was not found.");
   res.send(course);
