bb806fd12835d26d420db72bd99197b1158c8c80
om pandey <om.pandey@aircall.io>
2022-01-24 14:03:08 +0530

    modularity on entry file

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,13 @@
 import express from "express";
+import coursesRouter from "./modules/courses/cources.routes";
 
 const app = express();
 
 app.use(express.json());
-const courses: any = [
-  { id: 1, name: "course1" },
-  { id: 2, name: "course2" },
-  { id: 3, name: "course3" },
-];
+app.use("/api/courses", coursesRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
-});
-
-app.get("/api/courses", (req, res) => {
-  res.send(courses);
-});
-
-app.post("/api/courses", (req, res) => {
-  const course = {
-    id: courses.length + 1,
-    name: req.body.name,
-  };
-  courses.push(course);
-  res.send(courses);
-});
-
-app.put("/api/courses/:id", (req, res) => {
-  const course = courses.find((c: any) => c.id === parseInt(req.params.id));
-  if (!course)
-    return res.status(404).send("The Course with given ID was not found.");
-
-  course.name = req.body.name;
-  res.send(course);
-});
-
-app.delete("/api/courses/:id", (req, res) => {
-  const course = courses.find((c: any) => c.id === parseInt(req.params.id));
-  if (!course)
-    return res.status(404).send("The Course with given ID was not found.");
-
-  const index = courses.indexOf(course);
-  courses.splice(index, 1);
-  res.send(course);
-});
-
-app.get("/api/courses/:id", (req, res) => {
-  const course = courses.find((c: any) => c.id === parseInt(req.params.id));
-  if (!course)
-    return res.status(404).send("The Course with given ID was not found.");
-  res.send(course);
 });
 
 // PORT
